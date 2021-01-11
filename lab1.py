3edc04422e09e10dd8c8e843872903128a2a9325
bui1@ualberta.ca <bui1@ualberta.ca>
2021-01-11 16:10:17 -0700

    Add citation for text attribute on requests

--- lab1.py
+++ lab1.py
@@ -1,3 +1,7 @@
 import requests
 
+// Reading text content from a http request
+// stolen from Jortega https://stackoverflow.com/users/7551712/jortega
+// From StackOverflow
+// From https://stackoverflow.com/a/58036567
 print(requests.get("https://raw.githubusercontent.com/bui1/cmput404lab1/master/lab1.py").text)
