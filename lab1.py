76a875f227d3f8a9ff5bd1ba09aae9cf35c7ae03
bui1@ualberta.ca <bui1@ualberta.ca>
2021-01-11 18:33:48 -0700

    Fixed comment syntax and forgot to add version printing of module

--- lab1.py
+++ lab1.py
@@ -1,7 +1,13 @@
 import requests
 
-// Reading text content from a http request
-// stolen from Jortega https://stackoverflow.com/users/7551712/jortega
-// From StackOverflow
-// From https://stackoverflow.com/a/58036567
+# How to print the version of an installed module
+# stolen from damienfrancois https://stackoverflow.com/users/1763614/damienfrancois
+# From StackOverflow
+# From https://stackoverflow.com/a/20180564
+print(requests.__version__)
+
+# Reading text content from a http request
+# stolen from Jortega https://stackoverflow.com/users/7551712/jortega
+# From StackOverflow
+# From https://stackoverflow.com/a/58036567
 print(requests.get("https://raw.githubusercontent.com/bui1/cmput404lab1/master/lab1.py").text)
